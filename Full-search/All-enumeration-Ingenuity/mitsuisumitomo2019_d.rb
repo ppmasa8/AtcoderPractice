cb9db16a19ce35e3ef095213df38890b3a3489d9
ppmasa8 <kiyokiyopip56@gmail.com>
2021-04-23 00:30:34 +0900

    refactoring

--- Full-search/All-enumeration-Ingenuity/mitsuisumitomo2019_d.rb
+++ Full-search/All-enumeration-Ingenuity/mitsuisumitomo2019_d.rb
@@ -3,10 +3,10 @@ s = gets.chomp
 def main(s)
   ans = 0
   ('000'..'999').each do |i|
-    (a = s.index(i[0], 0)) &&
-      (b = s.index(i[1], a + 1)) &&
-      (c = s.index(i[2], b + 1))
-    ans += 1 if a && b && c
+    (a = s.index(i[0], 0))
+    (b = s.index(i[1], a + 1)) unless a.nil?
+    (c = s.index(i[2], b + 1)) unless b.nil?
+    ans += 1 if c
   end
 end
 
