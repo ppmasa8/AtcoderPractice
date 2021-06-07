2aa9e248662cc54b38a37935a25ff1980b1fc755
ppmasa8 <kiyokiyopip56@gmail.com>
2021-06-07 20:37:51 +0900

    Update binary_search.rb

--- binary_search.rb
+++ binary_search.rb
@@ -2,13 +2,15 @@
 def binary_search(a,key) 
   lo  = 0
   hi  = a.size - 1
-  mid = lo + (lo + hi) / 2
 
-  if a[mid] == key
-    return a[mid]
-  elsif a[mid] < key
-    lo = mid + 1
-  else
-    hi = mid - 1
+  while lo < hi
+    mid = lo + (lo + hi) / 2
+    if a[mid] == key
+      return a[mid]
+    elsif a[mid] < key
+      lo = mid + 1
+    else
+      hi = mid - 1
+    end
   end
 end
