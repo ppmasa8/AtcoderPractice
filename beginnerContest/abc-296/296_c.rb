a6dd7ee2d0f766103ce8792639f9f982de06dc32
ppmasa8 <kiyokiyopip56@gmail.com>
2023-04-13 17:30:27 +0900

    Update 296_c.rb

--- beginnerContest/abc-296/296_c.rb
+++ beginnerContest/abc-296/296_c.rb
@@ -11,3 +11,5 @@ s.each do |num|
   end
 end
 puts "No"
+
+### Set#include?とArray#include?の実行速度が10倍くらい違う理由 → https://qiita.com/an_sony/items/708c47d073ad709431d6#%E3%81%A7%E3%81%AA%E3%82%93%E3%81%A7%E9%80%9F%E3%81%84%E3%81%AE%E3%82%88
